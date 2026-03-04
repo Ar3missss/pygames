e967d541826ff5fb38119cfa28c617adab76a069
Ar3missss <pathaniaaryan169@gmail.com>
2026-03-04 22:44:27 +0530

    stars

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 import random
+pygame.font.init()
 
 WIDTH,HEIGHT=1000,600
 WIN=pygame.display.set_mode((WIDTH,HEIGHT))
@@ -10,9 +11,14 @@ BG=pygame.transform.scale(pygame.image.load("dodge.jpeg"),(WIDTH,HEIGHT))
 
 PLAYER_WIDTH=40
 PLAYER_HEIGHT=60
+PLAYER_VELOCITY=5
 
-def draw(player):
+FONT=pygame.font.SysFont("comicsans",30)
+
+def draw(player,elapsed_time):
     WIN.blit(BG,(0,0))
+    time_text=FONT.render(f"Time:{round(elapsed_time)}s",1,"white")
+    WIN.blit(time_text,(10,10))
     pygame.draw.rect(WIN,'red',player)
     pygame.display.update()
 
@@ -20,14 +26,27 @@ def main():
     run = True
 
     player=pygame.Rect(200,HEIGHT-PLAYER_HEIGHT, PLAYER_WIDTH,PLAYER_HEIGHT)
+    clock=pygame.time.Clock()
+    start_time=time.time()
+    elapsed_time=0
 
     while run:
+        clock.tick(120)
+        elapsed_time=time.time()-start_time
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run=False
                 break
-        draw(player)    
 
+        keys=pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player.x-PLAYER_VELOCITY>=0:
+            player.x -= PLAYER_VELOCITY
+        if keys[pygame.K_RIGHT] and player.x + PLAYER_VELOCITY + PLAYER_WIDTH<=WIDTH:
+            player.x += PLAYER_VELOCITY  
+
+        draw(player,elapsed_time)    
+ 
     pygame.quit() 
 
 if __name__=="__main__":
