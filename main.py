d2e07c5662ea32fc15e81725114091a131df8d5e
Ar3missss <pathaniaaryan169@gmail.com>
2026-03-08 01:31:11 +0530

    loop

--- main.py
+++ main.py
@@ -9,26 +9,37 @@ BACKGROUND_COLOR=(0,0,0)
 RED= (255,0,0)
 BLUE= (0,0,255)
 SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
-PLAYER=pygame.Rect(random.randint(0,WIDTH),HEIGHT-50,50,50)
-ENEMY=pygame.Rect(random.randint(0,WIDTH),0,50,50)
+PLAYER=pygame.Rect(WIDTH/2,HEIGHT-50,50,50)
+ENEMY=pygame.Rect(random.randint(0,WIDTH-50),0,50,50)
 
 game_over=False
+clock= pygame.time.Clock()
 
 while not game_over: 
+    clock.tick(30)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                PLAYER.x -= 50
+                PLAYER.x -= 50 
                      
             elif event.key ==pygame.K_RIGHT:
                 PLAYER.x += 50
 
+    ENEMY.y +=5
+    if ENEMY.y>HEIGHT:
+        ENEMY.y = 0
+        ENEMY.x = random.randint(0,WIDTH-50)               
+
+
     SCREEN.fill(BACKGROUND_COLOR)       
     pygame.draw.rect(SCREEN,RED,ENEMY)        
-    pygame.draw.rect(SCREEN,BLUE,PLAYER)        
+    pygame.draw.rect(SCREEN,BLUE,PLAYER) 
+
+          
 
     pygame.display.update()
         
