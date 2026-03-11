313e981ec94222160de36cf1693c6a8967d07a43
Ar3missss <pathaniaaryan169@gmail.com>
2026-03-11 22:20:36 +0530

    delay

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ BACKGROUND_COLOR=(0,0,0)
 RED= (255,0,0)
 BLUE= (0,0,255)
 SPEED_Y=5
-SPEED_X=25
+SPEED_X=50
 SCREEN = pygame.display.set_mode((WIDTH,HEIGHT))
 CAPTION = pygame.display.set_caption('Dodge Blocks')
 PLAYER=pygame.Rect(WIDTH/2,HEIGHT-50,50,50)
@@ -18,16 +18,20 @@ SCORE=0
 ENEMIES=[]
 NUM_ENEMY=5
 for _ in range (NUM_ENEMY):
-        x = random.randint(0,WIDTH-50)
-        y = 0
-        enemy=pygame.Rect(x,y,50,50)
-        ENEMIES.append(enemy)
+         x = random.randint(0,WIDTH-50)
+         y = 0
+         enemy=pygame.Rect(x,y,50,50)
+         ENEMIES.append(enemy)
 
 game_over=False
 clock= pygame.time.Clock()
 
+spawn_timer = 0
+spawn_delay = 2000
+
 # Loop
 while not game_over: 
+    dt=clock.tick(60)
     clock.tick(30)
     SCORE += 1
     SPEED_Y = 5 + SCORE // 100
@@ -36,13 +40,19 @@ while not game_over:
         if event.type == pygame.QUIT:
             sys.exit()
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT and PLAYER.x - SPEED_X >=0:
-                PLAYER.x -= SPEED_X
+        keys = pygame.key.get_pressed()
+        if keys [pygame.K_LEFT] and PLAYER.x - SPEED_X >=0:
+            PLAYER.x -= SPEED_X
                      
-            elif event.key ==pygame.K_RIGHT and PLAYER.x + SPEED_X + PLAYER.width<=WIDTH:
-                PLAYER.x += SPEED_X
-
+        elif keys [pygame.K_RIGHT] and PLAYER.x + SPEED_X + PLAYER.width<=WIDTH:
+            PLAYER.x += SPEED_X
+    
+    spawn_timer += dt        
+    if spawn_timer > spawn_delay:
+        x = random.randint(0, WIDTH-50)
+        enemy = pygame.Rect(x, -50, 50, 50)
+        ENEMIES.append(enemy)
+        spawn_timer = 0
 
 # Updating Position of Enemy
     
